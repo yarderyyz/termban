333225822f2f0aa882faa9bc82382a1d8b0218bd
Colton Phillips <coltonjphillips@gmail.com>
2024-09-02 06:44:08 -0700

    Remove uneeded LOC

--- src/soko_loader.rs
+++ src/soko_loader.rs
@@ -181,8 +181,7 @@ fn parse_sokoban_level(tokens: Vec<Token>) -> Result<World, String> {
 
 pub fn parse_sokoban_worlds(sokoban_text: &str) -> Result<Vec<World>, String> {
     if let Some(tokens) = tokenize(sokoban_text) {
-        let sokoban_token_groups = group_sokoban_tokens(tokens);
-        let worlds: Vec<World> = sokoban_token_groups
+        let worlds: Vec<World> = group_sokoban_tokens(tokens)
             .iter()
             .flat_map(|level| parse_sokoban_level(level.clone()))
             .collect();
