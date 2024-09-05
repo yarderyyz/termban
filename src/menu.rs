69a8f9e3d0cce426765d958d14a11662255a21f3
Colton Phillips <coltonjphillips@gmail.com>
2024-09-04 22:11:40 -0700

    polish main page and let user press enter

--- src/menu.rs
+++ src/menu.rs
@@ -9,10 +9,32 @@ use ratatui::{
 };
 
 pub fn view(_model: &mut Model, frame: &mut Frame) {
-    frame.render_widget(
-        Paragraph::new("Press space to start\nPress esc to Quit".to_string()),
-        frame.area(),
+    render_widget_paragraph(
+        frame,
+        "
+TERMBAN SOKOBAN 1.0 ENGINE
+==========================
+INTRODUCING... MICROBAN I!
+        BY DAVID W. SKINNER
+
+CONTROLS:       Press Enter to Begin
+                Press Escape to Leave
+                Move Player Using WASD/Arrows
+                Press R to Restart Level
+                Press Z or U to Undo a Move
+
+RULES:
+    1. The PLAYER and BOXES can only occupy EMPTY or GOAL Tiles.
+    2. The PLAYER can push a BOX onto an EMPTY Tile.
+    3. A BOX cannot be pushed by another BOX.
+GOAL:
+    The puzzle is solved when every GOAL tile is occupied by a BOX.
+",
     );
+}
+
+fn render_widget_paragraph(frame: &mut Frame<'_>, paragraph: &str) {
+    frame.render_widget(Paragraph::new(paragraph.to_string()), frame.area());
 }
 
 pub fn update(model: &mut Model, msg: MenuAction) -> Option<MenuAction> {
@@ -45,7 +67,7 @@ pub fn handle_event(_: &Model) -> io::Result<Option<MenuAction>> {
 
 pub fn handle_key(key: event::KeyEvent) -> Option<MenuAction> {
     match key.code {
-        KeyCode::Char(' ') => Some(MenuAction::StartGame),
+        KeyCode::Enter | KeyCode::Char(' ') => Some(MenuAction::StartGame),
         KeyCode::Esc => Some(MenuAction::Quit),
         _ => None,
     }
