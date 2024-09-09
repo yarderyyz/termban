2235ac1ad89cebab8eb789db2816546b797868d4
Lee Gauthier <lee.d.gauthier@gmail.com>
2024-09-09 16:34:48 +0200

    clean up render_tests

--- src/render_tests.rs
+++ src/render_tests.rs
@@ -1,53 +1,52 @@
 #[cfg(test)]
-mod render_tests {
-    use crate::{
-        render::*,
-        types::{Coordinate, GlyphCells},
-    };
-    use ndarray::Array2;
+use crate::{
+    render::*,
+    types::{Coordinate, GlyphCells},
+};
+#[cfg(test)]
+use ndarray::Array2;
 
-    #[test]
-    fn coordinate_is_inside_the_bounds() {
-        let glyph_cells_buffer: GlyphCells = Array2::default((5, 5));
+#[test]
+fn coordinate_is_inside_the_bounds() {
+    let glyph_cells_buffer: GlyphCells = Array2::default((5, 5));
 
-        assert_eq!(
-            true,
-            is_in_bounds(&Coordinate { x: 1, y: 1 }, &glyph_cells_buffer)
-        );
+    assert!(is_in_bounds(
+        &Coordinate { x: 1, y: 1 },
+        &glyph_cells_buffer
+    ));
 
-        assert_eq!(
-            true,
-            is_in_bounds(&Coordinate { x: 4, y: 4 }, &glyph_cells_buffer)
-        );
+    assert!(is_in_bounds(
+        &Coordinate { x: 4, y: 4 },
+        &glyph_cells_buffer
+    ));
 
-        assert_eq!(
-            true,
-            is_in_bounds(&Coordinate { x: 0, y: 4 }, &glyph_cells_buffer)
-        );
-    }
+    assert!(is_in_bounds(
+        &Coordinate { x: 0, y: 4 },
+        &glyph_cells_buffer
+    ));
+}
 
-    #[test]
-    fn coordinate_is_outside_the_bounds() {
-        let glyph_cells_buffer: GlyphCells = Array2::default((5, 5));
+#[test]
+fn coordinate_is_outside_the_bounds() {
+    let glyph_cells_buffer: GlyphCells = Array2::default((5, 5));
 
-        assert_eq!(
-            false,
-            is_in_bounds(&Coordinate { x: 100, y: 100 }, &glyph_cells_buffer)
-        );
+    assert!(is_in_bounds(
+        &Coordinate { x: 100, y: 100 },
+        &glyph_cells_buffer
+    ));
 
-        assert_eq!(
-            false,
-            is_in_bounds(&Coordinate { x: 4, y: 100 }, &glyph_cells_buffer)
-        );
+    assert!(is_in_bounds(
+        &Coordinate { x: 4, y: 100 },
+        &glyph_cells_buffer
+    ));
 
-        assert_eq!(
-            false,
-            is_in_bounds(&Coordinate { x: 100, y: 1 }, &glyph_cells_buffer)
-        );
+    assert!(is_in_bounds(
+        &Coordinate { x: 100, y: 1 },
+        &glyph_cells_buffer
+    ));
 
-        assert_eq!(
-            false,
-            is_in_bounds(&Coordinate { x: 5, y: 1 }, &glyph_cells_buffer)
-        );
-    }
+    assert!(is_in_bounds(
+        &Coordinate { x: 5, y: 1 },
+        &glyph_cells_buffer
+    ));
 }
